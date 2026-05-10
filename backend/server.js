2b97d7933fca88abda85519ffb634ef8b99f81a5
dh4wall <dhawalshinde14@gmail.com>
2026-05-10 19:23:06 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const postRoutes = require('./routes/posts');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const path = require("path");
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/posts', postRoutes);
+
+// Root route
+/* app.get('/', (req, res) => {
+  res.send('Blog API is running...');
+}); */
+
+// Serve React build
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
+
+// Connect to MongoDB and start server
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('✅ Connected to MongoDB Atlas');
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('❌ MongoDB connection error:', err.message);
+    process.exit(1);
+  });
